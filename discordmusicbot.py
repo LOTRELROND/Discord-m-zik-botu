f9feee28144d1613bd4c4fe3e11676e65d76ac05
mehmet kemal kayam <81799730+LOTRELROND@users.noreply.github.com>
2023-04-01 16:03:00 +0300

    Add files via upload

---
+++ discordmusicbot.py
@@ -0,0 +1,36 @@
+import discord
+from discord.ext import commands
+import youtube_dl
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.command()
+async def play(ctx, url):
+    song = os.path.isfile("song.mp3")
+    try:
+        if song:
+            os.remove("song.mp3")
+    except PermissionError:
+        await ctx.send("Wait for the current playing music to end or use the 'stop' command")
+        return
+
+    voice_channel = discord.utils.get(ctx.guild.voice_channels, name='General')
+    await voice_channel.connect()
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            os.rename(file, "song.mp3")
+    voice.play(discord.FFmpegPCMAudio("song.mp3"))
+
+bot.run('your-bot-token-goes-here')
